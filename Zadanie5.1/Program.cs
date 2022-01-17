bad6a50410b5e69b704a578ca7fd8ba06c402f57
kamal673 <kamal673@yandex.ru>
2022-01-17 21:19:39 +0300

    Добавьте файлы проекта.

---
+++ Zadanie5.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВедите 7 сифр");
+            string s = Console.ReadLine();
+            string[] a = s.Split(' ');
+            double x = 0;
+            foreach (var i in a)
+            {
+                x += Convert.ToInt32(i);
+            }
+            double sr = x / a.Length;
+            Console.WriteLine(sr);
+        }
+    }
+}
+/*
+int[] array = new int[7] {2,4,5,6,7,8,9};
+*/
